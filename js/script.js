25ec29a6c7721f6316a01891c5cf599ccf587de7
vaibhavpatil2069 <91780916+vaibhavpatil2069@users.noreply.github.com>
2022-07-24 23:37:13 +0530

    pureHtml
    PureHTMLcoding

---
+++ js/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+$('.banner-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    adaptiveHeight: true
+  });
+  $('.responsive').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+});
+ 
